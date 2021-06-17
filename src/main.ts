2880a08291075d968e80060e4dd97211a1a85aec
RedCarrot0803 <redcarrot0803@gmail.com>
2021-06-17 20:04:55 +0900

    doc: install open api swagger document

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('R-grown Document')
+    .setDescription('The R-grown API description')
+    .setVersion('1.0')
+    .addTag('R-grown')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
   await app.listen(3000);
 }
 bootstrap();
